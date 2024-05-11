abfc446e23840af27e221c160228decdd4cd01df
yevhenii-bezeha <bezegajenja0305@gmail.com>
2024-05-11 22:16:58 +0200

    HW1

---
+++ src/contacts.js
@@ -0,0 +1,42 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((item) => item.id === contactId);
+  return contact || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index !== -1) {
+    const [removedContact] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return removedContact;
+  }
+  return null;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: Date.now().toString(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
